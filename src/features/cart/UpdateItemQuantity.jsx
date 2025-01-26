e91278f7acb548f5cb58a40baacb53dcdb7ee52b
Salah Hossam <salah3hossam@gmail.com>
2025-01-26 16:52:24 +0200

    Initial

---
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -0,0 +1,21 @@
+import { useDispatch } from 'react-redux';
+import Button from '../../ui/Button';
+import { decreaseItemQuan, increaseItemQuan } from './cartSlice';
+
+function UpdateItemQuantity({ pizzaId,currentQuantity }) {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex items-center gap-2 md:gap-3">
+      <Button type="round" onClick={() => dispatch(decreaseItemQuan(pizzaId))}>
+        -
+      </Button>
+      <span className='text-sm font-medium'>{currentQuantity}</span>
+      <Button type="round" onClick={() => dispatch(increaseItemQuan(pizzaId))}>
+        +
+      </Button>
+    </div>
+  );
+}
+
+export default UpdateItemQuantity;
